93106ab0bceb618c6dcfd5aac6b4469a0b9c1d0c
khaoula <khaoulajabour4@gmail.com>
2022-01-29 00:31:45 -0500

    conditionally rendered({{if}} helper)

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -91,13 +91,17 @@ router.post('/', (req, res)=>{
 });
 
 router.put('/upvote',(req, res)=>{
+    //make sure the session exist first
+    if (req.session) {
+        Post.upvote({...req.body, user_id: req.session.user_id}, {Vote, Comment,User})
+        .then(updatedPostData => res.json(updatedPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(400).json(err);
+        });
+    }
     // custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-    console.log(err);
-    res.status(400).json(err);
-    });
+
 
     // Vote.create({
     //     user_id: req.body.user_id,
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,7 +29,7 @@ router.get('/', (req, res)=>{
     }).then(dbPostData =>{
         
         const posts = dbPostData.map(post=>post.get({plain : true}));
-        res.render('homepage',{posts});
+        res.render('homepage',{posts, loggedIn: req.session.loggedIn});
         //console.log(dbPostData[0]);
     }).catch(err=>{
         console.log(err);
@@ -46,6 +46,49 @@ router.get('/login', (req, res)=>{
     res.render('login');
 });
 
+
+router.get('/post/:id' , (req, res)=>{
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: ['id', 'post_url', 'title', 'created_at',
+  [sequelize.literal('(select count(*) from vote where post.id =vote.post_id)'), 'vote_count']
+  ],
+  include:[
+    {
+      model: Comment,
+      attributes:['id', 'comment_text', 'post_id', 'created_at'],
+      include: {
+        model: User,
+        attributes:['username']
+      }
+    },
+    {
+      model: User,
+      attributes: ['username']
+    }
+  ]
+}).then(dbPostData => {
+    if (!dbPostData) {
+      res.status(404).json({ message: 'No post found with this id' });
+      return;
+    }
+
+    // serialize the data
+    const post = dbPostData.get({ plain: true });
+
+    // pass data to template
+    res.render('single-post', { 
+      post,
+      loggedIn: req.session.loggedIn
+     });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
 
 
